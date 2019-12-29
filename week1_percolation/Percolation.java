7029be5a7c5d53bd7bf6e41089245bbd210d1095
travis <travis.fu@hotmail.com>
2019-12-29 18:39:12 +0000

    implement percolation class constructor

--- week1_percolation/Percolation.java
+++ week1_percolation/Percolation.java
@@ -4,9 +4,24 @@
  *  Description: https://coursera.cs.princeton.edu/algs4/assignments/percolation/specification.php
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
+    private boolean[] openSites;
+    private WeightedQuickUnionUF grid;
+    private int virtualTop;
+    private int virtualBot;
+
     // creates n-by-n grid, with all sites initially blocked
-    // public Percolation(int n)
+    // grid also includes a virtual top and bot site, which all top and bottom
+    // rows are connected to via a union.
+    public Percolation(int n) {
+        int gridSize = n * n + 2; // +2 for virtual top and bot
+        openSites = new boolean[gridSize];
+        grid = new WeightedQuickUnionUF(gridSize);
+        virtualTop = 0;
+        virtualBot = gridSize - 1;
+    }
 
     // opens the site (row, col) if it is not open already
     // public void open(int row, int col)
