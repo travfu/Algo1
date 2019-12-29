edcefa733cf7428896d3195ec6c8e6138bc1acba
travis <travis.fu@hotmail.com>
2019-12-29 09:56:27 +0000

    percolation specs

---
+++ week1_percolation/Percolation.java
@@ -0,0 +1,30 @@
+/* *****************************************************************************
+ *  Name: Percolation
+ *  Date: 29 December 2019
+ *  Description: https://coursera.cs.princeton.edu/algs4/assignments/percolation/specification.php
+ **************************************************************************** */
+
+public class Percolation {
+    // creates n-by-n grid, with all sites initially blocked
+    // public Percolation(int n)
+
+    // opens the site (row, col) if it is not open already
+    // public void open(int row, int col)
+
+    // is the site (row, col) open?
+    // public boolean isOpen(int row, int col)
+
+    // is the site (row, col) full?
+    // public boolean isFull(int row, int col)
+
+    // returns the number of open sites
+    // public int numberOfOpenSites()
+
+    // does the system percolate?
+    // public boolean percolates()
+
+    // test client (optional)
+    public static void main(String[] args) {
+
+    }
+}
---
+++ week1_percolation/PercolationStats.java
@@ -0,0 +1,27 @@
+/* *****************************************************************************
+ *  Name: PercolationStats
+ *  Date: 29 December 2019
+ *  Description: https://coursera.cs.princeton.edu/algs4/assignments/percolation/specification.php
+ **************************************************************************** */
+
+public class PercolationStats {
+    // perform independent trials on an n-by-n grid
+    // public PercolationStats(int n, int trials)
+
+    // sample mean of percolation threshold
+    // public double mean()
+
+    // sample standard deviation of percolation threshold
+    // public double stddev()
+
+    // low endpoint of 95% confidence interval
+    // public double confidenceLo()
+
+    // high endpoint of 95% confidence interval
+    // public double confidenceHi()
+
+    // test client (see below)
+    public static void main(String[] args) {
+
+    }
+}
