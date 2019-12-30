5e1a52bb22461a1959ec08c7c31a961b78c3fd22
travis <travis.fu@hotmail.com>
2019-12-30 21:23:43 +0000

    version 1.0

--- week1_percolation/Percolation.java
+++ week1_percolation/Percolation.java
@@ -4,19 +4,33 @@
  *  Description: https://coursera.cs.princeton.edu/algs4/assignments/percolation/specification.php
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 
 public class Percolation {
     private boolean[] openSites;
+    private int openCount;
     private WeightedQuickUnionUF grid;
-    private int N;
-    private int virtualTop;
-    private int virtualBot;
+    private WeightedQuickUnionUF fullSites;
+    private final int n;
+    private final int virtualTop;
+    private final int virtualBot;
 
     // creates n-by-n grid, with all sites initially blocked
-    // grid also includes a virtual top and bot site, which all top and bottom
-    // rows are connected to via a union.
     public Percolation(int n) {
+        // Two sets of WeightedQuickUnion (WQU) connectivity array and an array
+        // of open sites are used here to model percolation.
+
+        // Both WQU represents connection between all open sites.
+        // The first WQU contains two extra elements, a virtual top and bottom,
+        // to enhance performance by forgoing iterating throuh first and last
+        // rows to check if the system percolates. However, the virtual bottom
+        // will inadvertently connect all non-full open sites, making it
+        // incompatible for checking whether a site is full.
+        // To solve this, the second WQU contains a virtual top but not bottom.
+        // This models the connections between all open sites but only full open
+        // sites will share a root connection value with the virtual top.
         if (n <= 0) {
             throw new IllegalArgumentException("n must be > 0");
         }
@@ -24,9 +38,10 @@ public class Percolation {
         int gridSize = n * n + 2; // +2 for virtual top and bot
         openSites = new boolean[gridSize];
         grid = new WeightedQuickUnionUF(gridSize);
+        fullSites = new WeightedQuickUnionUF(gridSize - 1);
         virtualTop = 0;
         virtualBot = gridSize - 1;
-        N = n;
+        this.n = n;
     }
 
     // opens the site (row, col) if it is not open already
@@ -39,24 +54,28 @@ public class Percolation {
 
         int i = getIndexOf(row, col);
         openSites[i] = true;
+        openCount++;
 
-        // if site is in first row, unionise with virtual top
-        if (row == 1) {
-            grid.union(virtualTop, i);
-        }
-        // if site is in last row, unionise with virtual bot
-        else if (row == N) {
-            grid.union(i, virtualBot);
-        }
         // unionise with adjacent open sites (top, bot, left, right), if exists
         tryUnion(i, row - 1, col);
         tryUnion(i, row + 1, col);
         tryUnion(i, row, col - 1);
         tryUnion(i, row, col + 1);
+
+        // if site is in first row, unionise with virtual top
+        if (row == 1) {
+            grid.union(virtualTop, i);
+            fullSites.union(virtualTop, i);
+        }
+
+        // if site is in last row and is full, unionise with virtual bot
+        if (row == n) {
+            grid.union(i, virtualBot);
+        }
     }
 
     private boolean validCoord(int row, int col) {
-        if (row > 0 && row <= N && col > 0 && col <= N) {
+        if (row > 0 && row <= n && col > 0 && col <= n) {
             return true;
         }
         return false;
@@ -64,9 +83,9 @@ public class Percolation {
 
     private int getIndexOf(int row, int col) {
         // Converts a 2-D matrix coord into a 1-D array index
-        // Note: arrays contains 2 extra elements at start (virtual top) and
-        //       end (virtual bot) of array.
-        int index = N * (row - 1) + col;
+        // Note: arrays contains an extra elements at start (virtual top) and
+        //       may contain extra element at end (virtual bot) of array.
+        int index = n * (row - 1) + col;
         return index;
     }
 
@@ -74,47 +93,45 @@ public class Percolation {
         if (validCoord(row, col) && isOpen(row, col)) {
             int index2 = getIndexOf(row, col);
             grid.union(index1, index2);
+            fullSites.union(index1, index2);
         }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        if (validCoord(row, col)) {
-            int i = getIndexOf(row, col);
-            return openSites[i];
-        }
-        else {
+        if (!validCoord(row, col)) {
             String msg = String.format("Coord(%s, %s) is out of range",
                                        row, col);
             throw new IllegalArgumentException(msg);
         }
+        int i = getIndexOf(row, col);
+        return openSites[i];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        if (validCoord(row, col)) {
-            int i = getIndexOf(row, col);
-            if (grid.find(i) == virtualTop) {
-                return true;
-            }
-            return false;
-        }
-        else {
+        if (!validCoord(row, col)) {
             String msg = String.format("Coord(%s, %s) is out of range",
                                        row, col);
             throw new IllegalArgumentException(msg);
         }
+        int i = getIndexOf(row, col);
+        if (fullSites.find(i) == fullSites.find(virtualTop)) {
+            return true;
+        }
+        return false;
     }
 
     // returns the number of open sites
-    // public int numberOfOpenSites()
+    public int numberOfOpenSites() {
+        return openCount;
+    }
 
     // does the system percolate?
     public boolean percolates() {
         // if virtual bottom site is connected to virtual top, then it
         // percolates
-        System.out.println(String.format("bot: %s", grid.find(virtualBot)));
-        if (grid.find(virtualBot) == virtualTop) {
+        if (grid.find(virtualBot) == grid.find(virtualTop)) {
             return true;
         }
         return false;
@@ -122,47 +139,14 @@ public class Percolation {
 
     // test client (optional)
     public static void main(String[] args) {
-        Percolation p = new Percolation(4);
-        int x2 = p.grid.find(2);
-        int x6 = p.grid.find(6);
-        int x7 = p.grid.find(7);
-        int x10 = p.grid.find(10);
-        int x11 = p.grid.find(11);
-        int x12 = p.grid.find(12);
+        In in = new In(args[0]);
 
-        System.out.println(x2);
-        System.out.println(x6);
-        System.out.println(x7);
-        System.out.println(x10);
-        System.out.println(x11);
-        System.out.println(x12);
-
-        p.open(1, 2); // 2
-        p.open(2, 2); // 6
-        p.open(2, 3); // 7
-        p.open(3, 3); // 11
-        p.open(3, 4); // 12
-        p.open(3, 2); // 10
-
-        x2 = p.grid.find(2);
-        x6 = p.grid.find(6);
-        x7 = p.grid.find(7);
-        x10 = p.grid.find(10);
-        x11 = p.grid.find(11);
-        x12 = p.grid.find(12);
-
-        System.out.println(x2);
-        System.out.println(x6);
-        System.out.println(x7);
-        System.out.println(x10);
-        System.out.println(x11);
-        System.out.println(x12);
-
-        System.out.println(p.isFull(3, 1));
-        System.out.println(p.isFull(3, 2));
-
-        System.out.println(String.format("percolates? %s", p.percolates()));
-        p.open(4, 2);
-        System.out.println(String.format("percolates? %s", p.percolates()));
+        int n = in.readInt();
+        Percolation p = new Percolation(n);
+        while (!in.isEmpty()) {
+            int i = in.readInt();
+            int j = in.readInt();
+            p.open(i, j);
+        }
     }
 }
