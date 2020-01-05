7f7dd6d5fd2d59c0fa69b66ab8a67506f824ed2d
travis <travis.fu@hotmail.com>
2020-01-05 19:43:42 +0000

    fixes for test fails

--- week1_percolation/PercolationStats.java
+++ week1_percolation/PercolationStats.java
@@ -9,13 +9,21 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
     private int[] threshold;
-    private final int T;
+    private final int trials;
     private final int n;
+    private final double confLevel = 1.96;
+
+    private double meanValue;
+    private double stddevValue;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("n > 0; T > 0");
+        }
+
         threshold = new int[trials];
-        T = trials;
+        this.trials = trials;
         this.n = n;
 
         for (int i = 0; i < trials; i++) {
@@ -38,29 +46,48 @@ public class PercolationStats {
     public double mean() {
         double mean = StdStats.mean(threshold);
         int totalSites = n * n;
-        return mean / totalSites;
+        double sampleMean = mean / totalSites;
+        meanValue = sampleMean;
+        return sampleMean;
     }
+
+    private double getMean() {
+        if (meanValue != 0.0d) {
+            return meanValue;
+        }
+        return mean();
+    }
+
 
     // sample standard deviation of percolation threshold
     public double stddev() {
         double stddev = StdStats.stddev(threshold);
         int totalSites = n * n;
-        return stddev / totalSites;
+        double sampleStddev = stddev / totalSites;
+        stddevValue = sampleStddev;
+        return sampleStddev;
+    }
+
+    private double getStddev() {
+        if (stddevValue != 0.0d) {
+            return stddevValue;
+        }
+        return stddev();
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        double mean = mean();
-        double stddev = stddev();
-        double loCI = mean - (1.96 * stddev) / Math.sqrt(T);
+        double mean = getMean();
+        double stddev = getStddev();
+        double loCI = mean - (confLevel * stddev) / Math.sqrt(trials);
         return loCI;
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        double mean = mean();
-        double stddev = stddev();
-        double hiCI = mean + (1.96 * stddev) / Math.sqrt(T);
+        double mean = getMean();
+        double stddev = getStddev();
+        double hiCI = mean + (confLevel * stddev) / Math.sqrt(trials);
         return hiCI;
     }
 
